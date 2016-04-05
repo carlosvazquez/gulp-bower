5672bc0e5635e9479dcfc6c8354df779357f4cab
Carlos Vazquez <carlosvazquezmurillo@gmail.com>
2016-04-05 05:24:03 -0500

    prerelease

--- public/curso-atm.php
+++ public/curso-atm.php
@@ -20,41 +20,57 @@
 
 
 
-  <section id="anchor-point" class="books">
-    <div class="container">
-        <div class="page-header col-lg-10 col-lg-offset-1">
-        <h1>Curso de ATM</h1>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-lg-10 col-lg-offset-1">
-          <p class="lead">Curso práctico para aprender a armonizar melodías con un método único y fácil, en un contexto moderno y creativo. Un recurso invaluable que todo músico debe poseer. Utilizamos el sistema del Conservatorio Juilliard de Nueva York.</p>
-          <p>Curso fundamental para músicos con interés en composición, arreglos y producciones musicales, improvisación, ingeniería de sonido, pianistas o músicos que toquen algún instrumento armónico ya sea como principiantes o profesionales quienes podrán ahora utilizar sus habilidades de ejecución con un propósito más creativo pudiendo así expresar su espontaneidad e inventiva personales.</p>
-          <p>El profesor Cèsar de la Cerda pianista y arreglista, reconocido internacionalmente por ser autor de los libros Armonia Tonal Moderna (Modern Tonal Harmony), El Arreglo para Piano (Arranging for the Piano) e Improvisación para Piano (Piano Improvisation) ha diseñado a través de 40 años de experiencia en la enseñanza el material teórico de este curso basado en los conceptos armónicos de muchos autores contemporáneos, principalmente de su mentor John Mehegan ("Jazz Improvisation", Conservatorio Juilliard de Nueva York), aunque el tema del curso es tratado con originalidad y contiene una gran variedad de aspectos modernos e innovadores. El curso utiliza la terminología conocida como jazz harmony o armonía jazzística aunque el propósito es entender de una manera sencilla y clara esa armonía al mismo tiempo que utilizarla en cualquier género de música. La finalidad de este curso es que a su término el músico sea capaz de armonizar y rearmonizar cualquier melodía, independientemente del género, en un contexto moderno y creativo. <a href="/autor">Click para saber más del AUTOR</a></p>
-          <h3>El curso está dirigido a:</h3>
-          <ol>
-            <li>El estudiante o el músico profesional que deseen obtener una información actualizada, completa y organizada de los conceptos modernos de la armonía tal como se les emplea en las melodías populares internacionales, la música de jazz y cualquier género de composición musical.</li>
-            <li>El arreglista o compositor interesados en experimentar nuevos recursos armónicos y de composición pero que se encuentra frecuentemente limitado por la falta de conocimiento en técnicas modernas</li>
-            <li>El maestro de enseñanza musical (piano, teclado, etc.), quien podrá encontrar en las lecciones del Curso fundamentos importantes para su trabajo profesional.</li>
-            <li>El aficionado deseoso de incrementar sus conocimientos de un modo rápido y práctico, que le ahorre largos y trabajosos estudios.</li>
-          </ol>
-          <h3>CONTENIDO</h3>
-          <p>En solo 18 lecciones usted podrá aprender todo lo que necesita saber sobre armonía tonal moderna. El curso completo está dividido en 3 unidades de 6 lecciones cada una y está diseñado para estudiarse en un lapso de 6 a 8 meses dependiendo del alumno. El estudio por unidad es de 2 meses pero el alumno tiene la libertad de ir a su propio ritmo.</p>
-          <p>El curso incluye, además de sus lecciones en PDF y videos ilustrativos, la práctica de 34 melodías del repertorio internacional, con sus correspondientes soluciones, numerosas tablas y diagramas, ilustraciones de fragmentos melódicos así como ejercicios prácticos que ayudarán al estudiante a reforzar los conocimientos adquiridos durante su aprendizaje.</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12 col-lg-10 col-lg-offset-1">
-          <h2>Asesorias Personalizadas</h2>
-          <p class="lead">Para todos nuestros alumnos, contamos con servicio de asesorías personalizadas para cualquiera de sus dudas surgidas durante el estudio del Curso como apoyo adicional para su desarrollo musical profesional. Las asesorías son a través de email</p>
-        </div>
-        <div class="col-xs-12 col-lg-10 col-lg-offset-1 text-center">
-        <h3>CERTIFICADO DE RECONOCIMIENTO</h3>
-        <p class="lead"><em>Al concluir el Curso de Armonía Tonal Moderna recibirá un certificado personalizado que avalará sus estudi</em></p>
-        </div>
+<section id="anchor-point" class="curso-atm">
+  <div class="container">
+      <div class="page-header col-lg-10 col-lg-offset-1">
+      <h1>Curso de ATM</h1>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-lg-10 col-lg-offset-1">
+        <p class="lead">Curso práctico para aprender a armonizar melodías con un método único y fácil, en un contexto moderno y creativo. Un recurso invaluable que todo músico debe poseer. Utilizamos el sistema del Conservatorio Juilliard de Nueva York.</p>
+        <p>Curso fundamental para músicos con interés en composición, arreglos y producciones musicales, improvisación, ingeniería de sonido, pianistas o músicos que toquen algún instrumento armónico ya sea como principiantes o profesionales quienes podrán ahora utilizar sus habilidades de ejecución con un propósito más creativo pudiendo así expresar su espontaneidad e inventiva personales.</p>
+        <p>El profesor Cèsar de la Cerda pianista y arreglista, reconocido internacionalmente por ser autor de los libros Armonia Tonal Moderna (Modern Tonal Harmony), El Arreglo para Piano (Arranging for the Piano) e Improvisación para Piano (Piano Improvisation) ha diseñado a través de 40 años de experiencia en la enseñanza el material teórico de este curso basado en los conceptos armónicos de muchos autores contemporáneos, principalmente de su mentor John Mehegan ("Jazz Improvisation", Conservatorio Juilliard de Nueva York), aunque el tema del curso es tratado con originalidad y contiene una gran variedad de aspectos modernos e innovadores. El curso utiliza la terminología conocida como jazz harmony o armonía jazzística aunque el propósito es entender de una manera sencilla y clara esa armonía al mismo tiempo que utilizarla en cualquier género de música. La finalidad de este curso es que a su término el músico sea capaz de armonizar y rearmonizar cualquier melodía, independientemente del género, en un contexto moderno y creativo. <a href="/autor">Click para saber más del AUTOR</a></p>
       </div>
     </div>
-  </section>
-<section>
+  </div>
+</section>
+
+<section id="dirigido">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-lg-10 col-lg-offset-1">
+        <h3>El curso está dirigido a:</h3>
+        <ol>
+          <li>El estudiante o el músico profesional que deseen obtener una información actualizada, completa y organizada de los conceptos modernos de la armonía tal como se les emplea en las melodías populares internacionales, la música de jazz y cualquier género de composición musical.</li>
+          <li>El arreglista o compositor interesados en experimentar nuevos recursos armónicos y de composición pero que se encuentra frecuentemente limitado por la falta de conocimiento en técnicas modernas</li>
+          <li>El maestro de enseñanza musical (piano, teclado, etc.), quien podrá encontrar en las lecciones del Curso fundamentos importantes para su trabajo profesional.</li>
+          <li>El aficionado deseoso de incrementar sus conocimientos de un modo rápido y práctico, que le ahorre largos y trabajosos estudios.</li>
+        </ol>
+        <h3>Contenido</h3>
+        <p>En solo 18 lecciones usted podrá aprender todo lo que necesita saber sobre armonía tonal moderna. El curso completo está dividido en 3 unidades de 6 lecciones cada una y está diseñado para estudiarse en un lapso de 6 a 8 meses dependiendo del alumno. El estudio por unidad es de 2 meses pero el alumno tiene la libertad de ir a su propio ritmo.</p>
+        <p>El curso incluye, además de sus lecciones en PDF y videos ilustrativos, la práctica de 34 melodías del repertorio internacional, con sus correspondientes soluciones, numerosas tablas y diagramas, ilustraciones de fragmentos melódicos así como ejercicios prácticos que ayudarán al estudiante a reforzar los conocimientos adquiridos durante su aprendizaje.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="asesorias">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-lg-10 col-lg-offset-1">
+        <p><strong>Asesorias Personalizadas</strong></p>
+        <p>Para todos nuestros alumnos, contamos con servicio de asesorías personalizadas para cualquiera de sus dudas surgidas durante el estudio del Curso como apoyo adicional para su desarrollo musical profesional. Las asesorías son a través de email</p>
+      </div>
+      <div class="col-xs-12 col-lg-10 col-lg-offset-1 text-center">
+        <p><img src="images/certificate-es.png" alt="" /></p>
+      <h2>CERTIFICADO DE RECONOCIMIENTO</h2>
+      <p class="lead"><em>Al concluir el Curso de Armonía Tonal Moderna recibirá un certificado personalizado que avalará sus estudios.</em></p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="planestudios">
   <div class="container">
     <div class="row">
       <div class="page-header col-xs-12 col-lg-10 col-lg-offset-1">
@@ -70,10 +86,56 @@
         <li><strong>LECCION 5: PROGRESION DIA TONICA Y PROGRESION CROMATICA.</strong> Uso Alternativo de las Progresiones. Análisis Armónico.</li>
         <li><strong>LECCION 6: INTERACCION DINAMICA DE LOS DOS POLOS DE LA TONALIDAD.</strong> La Relación Bipolar I-V. Reemplazo del Acorde VI en la Progresión I VI II V. Importancia del Centro Tonal.</li>
       </ul>
-      <h4></h4>
+      <h4>SEGUNDA UNIDAD</h4>
+      <ul>
+        <li><strong>LECCION 7: ALTERACIONES EXPRESIVAS EN LOS ACORDES, Suspensión del Acorde Menor.</strong> Variantes del Acorde Dominante.</li>
+        <li><strong>LECCION 8: SUSTITUCION DEL ACORDE DOMINANTE.</strong> Transformación de la Progresión de Quintas en Progresión Cromática.</li>
+        <li><strong>LECCION 9: REEMPLAZO DEL ACORDE DOMINANTE.</strong> Resoluciones Excepcionales en el Acorde I. Acorde Dominante sin Preparación.</li>
+        <li><strong>LECCION 10: ACORDES EST ACIONARIOS Y ACORDES P ARALELOS.</strong></li>
+        <li><strong>LECCION 11: ACORDES SINONIMOS E INVERSIONES.</strong></li>
+        <li><strong>LECCION 12: REARMONIZACION DE MELODIAS CON ARMONIA CIFRADA: Conversión.</strong> Corrección.</li>
+      </ul>
+      <h4>TERCERA UNIDAD</h4>
+      <ul>
+        <li><strong>LECCION 13: LAS ESCALAS DE LOS ACORDES (I): Modos Diatónicos.</strong></li>
+        <li><strong>LECCION 14: LAS ESCALAS DE LOS ACORDES (II): Escalas Artificiales: Escala Disminuida. Escala de Tonos Enteros.</strong> Escala Pentatónica. Escala Blues.</li>
+        <li><strong>LECCION 15: MODULACION: Cambios de Tonalidad en el Curso de la Melodía.</strong> Cambios de Tonalidad Deliberados.</li>
+        <li><strong>LECCION 16: LA TONALIDAD MENOR: La Triple Escala Menor.</strong> Los Siete Acordes de la Tonalidad Menor.</li>
+        <li><strong>LECCION 17: LAS EXTENSIONES DE LOS ACORDES: Análisis de los Intervalos.</strong> Estructura Básica y Extensiones. Alteraciones de las Extensiones.</li>
+        <li><strong>LECCION 18: METODOS DE DISTRIBUCION: Redistribución y Espaciamiento de Voces.</strong></li>
+      </ul>
     </div>
   </div>
 </section>
 
+<section id="mejorprecio">
+  <div class="container">
+    <div class="page-header col-xs-12 col-lg-10 col-lg-offset-1 text-center">
+      <h4>INSCRIBETE HOY AL CURSO COMPLETO Y LLEVATE GRATIS</h4>
+      <h2>ALBUM DE MELODIAS REHARMONIZADAS</h2>
+      <p class="lead">PRECIO NORMAL <strong>$8.50</strong></p>
+    </div>
+    <div class="col-xs-12 col-lg-10 col-lg-offset-1">
+      <p>El álbum contiene más de 40 melodías del repertorio internacional con su armonía original y su rearmonización dando a las melodías un nuevo color al cambiar su armonía básica y enriqueciéndola en un contexto más moderno. Este álbum provee de un recurso excelente para analizar el uso de las progresiones y la resolución de los problemas que se presentan en la práctica armónica. Es un excelente complemento educativo para todos aquellos que completaron el curso de Armonía Tonal Moderna ya que les permitirá reafirmar los conocimientos adquiridos. Incluye una guía para la interpretación de los símbolos armónicos.</p>
+      <h4>Las melodías del álbum están clasificadas en:</h4>
+      <p>a) tonalidades mayores</p>
+      <p>b) tonalidades menores</p>
+      <p>c) con cambio de tonalidad</p>
+    </div>
+  </div>
+</section>
+<section id="promoprecios">
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 text-center">
+      <h4>COSTOS</h4>
+      <p>Usted puede pagar por medio de su tarjeta de crédito o débito (Visa, Mastercard, etc.) de manera fácil y segura a través de PayPal. También puede realizar su pago por depósito bancario si usted se encuentra en México o Estados Unidos.</p>
+      <p>En caso de no contar con alguna tarjeta usted puede hacer su pago por medio de Western Union o algún medio similar.</p>
+      <h2>3 PAGOS MENSUALES DE <strong>$70 usd</strong></h3>
+      <h2>CURSO COMPLETO (Mejor Precio) <strong>$199 usd</strong></h3>
+    </div>
+  </div>
+</div>
+</section>
 
   <?php require_once 'include/footer.php'; ?>
